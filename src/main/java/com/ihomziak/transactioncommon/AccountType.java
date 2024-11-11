1232a195a9fe024026b1c4fc4a4faee0123ea5dc
ihomz <IVAN.HOMZIAK@ONEDATASCAN.COM>
2024-11-11 10:04:37 -0500

    init commit

---
+++ src/main/java/com/ihomziak/transactioncommon/AccountType.java
@@ -0,0 +1,16 @@
+package com.ihomziak.transactioncommon;
+
+public enum AccountType {
+    CHECKING("Checking"), SAVINGS("Savings"), CREDIT("Credit");
+
+    private final String accountType;
+
+    private AccountType(String accountType){
+        this.accountType = accountType;
+    }
+
+    @Override
+    public String toString() {
+        return this.accountType;
+    }
+}
---
+++ src/main/java/com/ihomziak/transactioncommon/TransactionType.java
@@ -0,0 +1,18 @@
+package com.ihomziak.transactioncommon;
+
+public enum TransactionType {
+    TRANSFER("TRANSFER"),
+    DEPOSIT("DEPOSIT"),
+    WITHDRAW("WITHDRAW");
+
+    private final String name;
+
+    private TransactionType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
