617b0b5599c623d9aa822a52f209e1dc01b17f5e
ivanhomziak <ivan.homziak@gmail.com>
2025-01-19 22:34:58 +0100

    ref

--- src/main/java/com/ihomziak/transactioncommon/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/ihomziak/transactioncommon/jwt/JwtAuthenticationFilter.java
@@ -13,39 +13,41 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.Collections;
 
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
-    private final JwtTokenProvider jwtTokenProvider;
-
-    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
-
-    @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain)
-            throws ServletException, IOException {
-
-        String token = getTokenFromRequest(request);
-
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            String username = jwtTokenProvider.getUsername(token);
-            String role = jwtTokenProvider.getRole(token);
-
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    username, null, Collections.singletonList(new SimpleGrantedAuthority(role)));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-
-        filterChain.doFilter(request, response);
-    }
-
-    private String getTokenFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
-    }
+public class JwtAuthenticationFilter
+//        extends OncePerRequestFilter
+{
+//
+//    private final JwtTokenProvider jwtTokenProvider;
+//
+//    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+//        this.jwtTokenProvider = jwtTokenProvider;
+//    }
+//
+//    @Override
+//    protected void doFilterInternal(@NonNull HttpServletRequest request,
+//                                    @NonNull HttpServletResponse response,
+//                                    @NonNull FilterChain filterChain)
+//            throws ServletException, IOException {
+//
+//        String token = getTokenFromRequest(request);
+//
+//        if (token != null && jwtTokenProvider.validateToken(token)) {
+//            String username = jwtTokenProvider.getUsername(token);
+//            String role = jwtTokenProvider.getRole(token);
+//
+//            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+//                    username, null, Collections.singletonList(new SimpleGrantedAuthority(role)));
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//        }
+//
+//        filterChain.doFilter(request, response);
+//    }
+//
+//    private String getTokenFromRequest(HttpServletRequest request) {
+//        String bearerToken = request.getHeader("Authorization");
+//        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+//            return bearerToken.substring(7);
+//        }
+//        return null;
+//    }
 }
--- src/main/java/com/ihomziak/transactioncommon/jwt/JwtTokenProvider.java
+++ src/main/java/com/ihomziak/transactioncommon/jwt/JwtTokenProvider.java
@@ -11,56 +11,56 @@ import java.util.Date;
 
 public class JwtTokenProvider {
 
-    private final SecretKey secretKey;
-    private final long validityInMilliseconds;
-
-    public JwtTokenProvider(String secretKey, long validityInMilliseconds) {
-        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
-        this.validityInMilliseconds = validityInMilliseconds;
-    }
-
-    public String createToken(String username, String role) {
-        Claims claims = Jwts.claims().setSubject(username);
-        claims.put("role", role);
-
-        Date now = new Date();
-        Date validity = new Date(now.getTime() + validityInMilliseconds);
-
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(secretKey, SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parserBuilder()
-                    .setSigningKey(secretKey)
-                    .build()
-                    .parseClaimsJws(token);
-            return true;
-        } catch (JwtException | IllegalArgumentException e) {
-            return false;
-        }
-    }
-
-    public String getUsername(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(secretKey)
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
-
-    public String getRole(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(secretKey)
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .get("role", String.class);
-    }
+//    private final SecretKey secretKey;
+//    private final long validityInMilliseconds;
+//
+//    public JwtTokenProvider(String secretKey, long validityInMilliseconds) {
+//        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
+//        this.validityInMilliseconds = validityInMilliseconds;
+//    }
+//
+//    public String createToken(String username, String role) {
+//        Claims claims = Jwts.claims().setSubject(username);
+//        claims.put("role", role);
+//
+//        Date now = new Date();
+//        Date validity = new Date(now.getTime() + validityInMilliseconds);
+//
+//        return Jwts.builder()
+//                .setClaims(claims)
+//                .setIssuedAt(now)
+//                .setExpiration(validity)
+//                .signWith(secretKey, SignatureAlgorithm.HS256)
+//                .compact();
+//    }
+//
+//    public boolean validateToken(String token) {
+//        try {
+//            Jwts.parserBuilder()
+//                    .setSigningKey(secretKey)
+//                    .build()
+//                    .parseClaimsJws(token);
+//            return true;
+//        } catch (JwtException | IllegalArgumentException e) {
+//            return false;
+//        }
+//    }
+//
+//    public String getUsername(String token) {
+//        return Jwts.parserBuilder()
+//                .setSigningKey(secretKey)
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody()
+//                .getSubject();
+//    }
+//
+//    public String getRole(String token) {
+//        return Jwts.parserBuilder()
+//                .setSigningKey(secretKey)
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody()
+//                .get("role", String.class);
+//    }
 }
