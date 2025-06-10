f3f42b84c2f4c6c9baa2553eb59d6b0945e1ed7c
IvanHomziak <ivan.homziak@gmail.com>
2025-06-10 16:35:17 +0200

    added BaseEntity

---
+++ src/main/java/com/ihomziak/bankingapp/common/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.ihomziak.bankingapp.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Base entity for common fields like ID, creation and update timestamps.
+ */
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedDate;
+}
