893a2fd37caba23ddf559834c707d500723b7ebd
IvanHomziak <ivan.homziak@gmail.com>
2025-06-20 17:11:52 +0200

    ref

--- src/main/java/com/ihomziak/bankingapp/common/dto/ErrorDTO.java
+++ src/main/java/com/ihomziak/bankingapp/common/dto/ErrorDTO.java
@@ -1,15 +1,14 @@
 package com.ihomziak.bankingapp.common.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorDTO {
     private String error;
-
-    public ErrorDTO(String error) {
-        this.error = error;
-    }
-
 }
